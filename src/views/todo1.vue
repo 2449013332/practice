652426524c6463b039ad5220158dd032066f712c
2449013332 <2449013332@qq.com>
2023-02-09 08:12:56 +0800

    add

--- src/views/todo1.vue
+++ src/views/todo1.vue
@@ -1,19 +1,59 @@
 <template>
-  <div>
-   <div   style="display:flex">
-          <el-input v-model="input" placeholder="请输入内容" :clearable="true"></el-input><el-button type="primary">添加</el-button>    
-   </div>
+  <div style="display: flex">
+    <div
+      v-for="(item, index) in divList"
+      :key="index"
+      class="div_box"
+      :class="{ orang: item.select == true }"
+      @click="change(item)"
+    >
+      {{ item.title }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
- data(){
-    return{
-        input: ''
-    }
- }
-/*
+  data() {
+    return {
+      divList: [
+        {
+          title: "1",
+          select: false,
+        },
+        {
+          title: "2",
+          select: false,
+        },
+        {
+          title: "3",
+          select: false,
+        },
+        {
+          title: "4",
+          select: false,
+        },
+        {
+          title: "5",
+          select: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    change(item) {
+      if (item.select) {
+        item.select = false;
+      } else {
+        // item.select=!item.select
+        this.divList.forEach((val) => {
+          val.select = false;
+        });
+        item.select = true;
+      }
+    },
+  },
+  /*
 1、新建一个输入框和添加按钮
 2、声明一个todoList,里面存对象，对象里有id,事项的名称，是否完成，v-for渲染在页面
 3、输入框输入待办事件，点击添加按钮，给todoList添加item【push】
@@ -24,11 +64,21 @@ export default {
 8、统计全部待办事件的数量就是用数组的length
 9、统计已完成的就是遍历数组筛选出已经完成的，装进一个新数组，用他的length
  */
-}
+};
 </script>
 
 <style>
-
+.div_box {
+  width: 200px;
+  height: 200px;
+  border: 1px solid black;
+  text-align: center;
+  line-height: 200px;
+  font-size: 20px;
+}
+.orang {
+  background: orange;
+}
 </style>
 
 
